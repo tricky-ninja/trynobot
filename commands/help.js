276f0fd89e9ae9ea4640b411db765149ea2fd60f
Xhackers7 <nishaajithkj@gmail.com>
2020-11-27 18:02:57 +0530

    Fixed some bugs

--- commands/help.js
+++ commands/help.js
@@ -38,7 +38,7 @@ data.push(`**Name:** ${command.name}`);
 
 if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 if (command.description) data.push(`**Description:** ${command.description}`);
-if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+if (command.usage) data.push(`**Usage:** &${command.name} ${command.usage}`);
 
 data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
