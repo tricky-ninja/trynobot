21e1cd94219b3453ce850b980d362f6b9d09f75a
Xhackers7 <nishaajithkj@gmail.com>
2020-11-27 17:35:56 +0530

    Added ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,12 @@
+
 module.exports = {
 	name: 'ping',
 	aliases: ['work', 'hi'],
 	description: 'Ping!',
 	cooldown: 5,
-	execute(message, args) {
-		message.channel.send('Pong!');
+	execute(message) {
+		const Discord = require('discord.js');
+		const client = new Discord.Client();
+		message.channel.send(`🏓Latency is \`${Date.now() - message.createdTimestamp}\`ms. API Latency is \`${Math.round(client.ws.ping)}\`ms`);
 	},
 };
