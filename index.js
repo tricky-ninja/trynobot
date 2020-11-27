43503a5310d8206e9a9bf383620376b2b63c094a
Xhackers7 <nishaajithkj@gmail.com>
2020-11-27 17:18:44 +0530

    wow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 //const { prefix, token } = require('./config.json');
-let prefix = '&';
+var prefix = '&';
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
