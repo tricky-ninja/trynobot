eb93c7cccac24afc8c84674e72ca65888a16f7b9
Xhackers7 <nishaajithkj@gmail.com>
2020-11-27 18:00:07 +0530

    Fixed some bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 //const { prefix, token } = require('./config.json');
-const prefix = '&';
+const prefix = '&'
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
