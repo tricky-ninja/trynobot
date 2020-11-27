431755ae01df384a916ea3bc15b6eb8e958ebc4d
Xhackers7 <nishaajithkj@gmail.com>
2020-11-27 18:01:09 +0530

    Fixed some bugs

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ const { commands } = message.client;
 if (!args.length) {
 	data.push('Here\'s a list of all my commands:');
 data.push(commands.map(command => command.name).join(', '));
-data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+data.push(`\nYou can send \`&help [command name]\` to get info on a specific command!`);
 
 return message.author.send(data, { split: true })
 	.then(() => {
