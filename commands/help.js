7f62e37a8974a78c5929da50dd229a0b782ec584
Xhackers7 <nishaajithkj@gmail.com>
2020-11-28 12:30:36 +0530

    Fixed help command

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,6 @@ module.exports = {
 	name: 'help',
 	aliases: ['h', 'start'],
 	description: 'List all of my commands or info about a specific command.',
-	aliases: ['commands'],
 	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args, prefix) {
