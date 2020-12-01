c7a376ab09c3f919f25cf52b01738b969e448cab
Xhackers7 <nishaajithkj@gmail.com>
2020-12-01 18:39:43 +0530

    Wania started

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', async message => {
+	d = Math.random();
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 const args = message.content.slice(prefix.length).trim().split(' ');
