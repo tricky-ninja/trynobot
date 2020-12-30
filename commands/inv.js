a15ca9d609c8f4ee44e0aaa3468d2652035a4ee4
Xhackers7 <nishaajithkj@gmail.com>
2020-12-30 17:27:12 +0530

    Optimized code

--- commands/inv.js
+++ commands/inv.js
@@ -1,19 +1,15 @@
 
 module.exports={
-    name: 'test',
-    description: 'makes you test',
+    name: 'inv',
+    description: 'makes you inv',
     devonly: true,
     execute(message, args, client, Discord) {
 // inside a command, event listener, etc.
 message.channel.send("wrking")
 message.author.send(client.guilds.cache.size)
 client.guilds.cache.forEach((guild) => {
-    const serverlist = new Discord.MessageEmbed()
-    .setTitle(`Tryno is in.`)
-    .addField(guild)
-    .setFooter(`Yo!`)
-    message.author.send(serverlist)
-    
+    guild.channels.cache.first().createInvite()
+    .then(inv => message.author.send(`${guild.name} | ${inv.url}`));
 })
     
 }
