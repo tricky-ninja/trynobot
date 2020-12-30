eba6e34afb54f4fc0b8d0bc9d1f6c524d65b9e3d
Xhackers7 <nishaajithkj@gmail.com>
2020-12-30 16:33:58 +0530

    Optimized code

--- commands/test.js
+++ commands/test.js
@@ -6,6 +6,13 @@ module.exports={
 // inside a command, event listener, etc.
 message.channel.send("wrking")
 message.author.send(client.guilds.cache.size)
+client.guilds.cache.forEach((guild) => {
+    const serverlist = new Discord.MessageEmbed()
+    .setTitle(`Tryno is in.`)
+    .addField(guild)
+    .setFooter(`Yo!`)
+    message.author.send(serverlist)
+})
     
 }
 }
