6bde2181d456095bf677195826dd48f4bb511d74
Xhackers7 <nishaajithkj@gmail.com>
2021-01-08 15:50:19 +0530

    Added database

--- commands/hug.js
+++ commands/hug.js
@@ -13,8 +13,14 @@ module.exports={
             return message.channel.send(`You didn't provide any mention anyone, ${message.author}!`);
         }
         const taggedUser = message.mentions.users.first();
-    
-    var wave = images.hug[Math.floor(Math.random() * images.hug.length)]
+    var hugs =  [
+        "https://media.tenor.com/images/ecf4840ba6fac22be773e586493d5283/tenor.gif", "https://media.tenor.com/images/bc8e962e6888249486a3e103edd30dd9/tenor.gif", 
+        "https://media.tenor.com/images/4d5a77b99ab86fc5e9581e15ffe34b5e/tenor.gif", "https://media.tenor.com/images/0a1652de311806ce55820a7115993853/tenor.gif",
+        "https://media.tenor.com/images/dedb92412ec281c9a89b79492fa71831/tenor.gif","https://media.tenor.com/images/cee298437607d7b123bc9664c9ce844b/tenor.gif",
+        "https://media.tenor.com/images/a9730f44f28d959abb4c5b31edc77de8/tenor.gif","https://media.tenor.com/images/1ca37ea5d3ec66ea08893d8679c04ae1/tenor.gif",
+        "https://media.tenor.com/images/1e058dc8d0ccd337b6d26cbab43b6e30/tenor.gif"
+    ]
+    var wave = images.hug[Math.floor(Math.random() * hugs.length)]
     
     var waveMsg = ["hugs", "is embaracing", "is hugging"]
 
--- commands/wave.js
+++ commands/wave.js
@@ -13,8 +13,13 @@ module.exports={
             return message.channel.send(`You didn't provide any mention anyone, ${message.author}!`);
         }
         const taggedUser = message.mentions.users.first();
-    
-    var wave = images.wave[Math.floor(Math.random() * images.wave.length)]
+    var waves = [
+        "https://media.tenor.com/images/8fd75e974da8aa9e506efad228ce5acc/tenor.gif","https://media.tenor.com/images/9aeadd5087b298b784a8aa7c44a2b70a/tenor.gif",
+    "https://media.tenor.com/images/4b9b18c7aae49b108354a22a0cb615fc/tenor.gif","https://media.tenor.com/images/250fc8aacb8c89b4b3b8a0384a3df4ea/tenor.gif",
+    "https://media.tenor.com/images/b56d184f2309f8e1f256205131a0f2e1/tenor.gif", "https://media.tenor.com/images/a7fda223af704afd4d68929f06009d0d/tenor.gif",
+    "https://media.tenor.com/images/e59de399e44d39ff55addf8d3a51da3a/tenor.gif", "https://media.tenor.com/images/fe3e2d08c49445ca807935eba60e5627/tenor.gif"
+    ]
+    var wave = images.wave[Math.floor(Math.random() * waves.length)]
     
     var waveMsg = ["says hi to", "is waving at", "says hello to"]
 
