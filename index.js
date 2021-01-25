1a3d20607f062cdfe533705303b1c10948cd4b49
Xhackers7 <nishaajithkj@gmail.com>
2021-01-25 16:46:16 +0530

    licence

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**********************************************Copyright***********************************************************
+									 This work is licensed under the
+			 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
+			 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+*******************************************************************************************************************/
+
 const fs = require('fs');
 const Discord = require('discord.js');
 const { setPriority } = require('os');
