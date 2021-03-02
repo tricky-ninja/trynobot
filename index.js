733b25fe63150aa1dcd9078fbe0368469155e2dc
Xhackers7 <73597485+Xhackers7@users.noreply.github.com>
2021-03-02 09:34:21 +0530

    Rivrob

--- index.js
+++ index.js
@@ -73,13 +73,14 @@ client.on('message', async message => {
 
 	const tuser = message.mentions.users.first();
 	const commandName = args.shift().toLowerCase();
-	const devonly = ['711074637689389127', '751736021661778004', '739502238032068659', '689845214273339589', '689418782934040592'];
+	const devonly = ['711074637689389127', '751736021661778004', '739502238032068659', '689845214273339589', '689418782934040592', '609476320266682547'];
 	const maindevs = ['711074637689389127', '751736021661778004']
 	const dev = client.users.cache.get('711074637689389127');
 	const dev2 = client.users.cache.get('751736021661778004');
 	const dev3 = client.users.cache.get('689845214273339589');
 	const dev4 = client.users.cache.get('689418782934040592');
 	const dev5 = client.users.cache.get('739502238032068659');
+        const dev6 = client.users.cache.get('609476320266682547');
 
 const command = client.commands.get(commandName)
 	|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
@@ -146,6 +147,7 @@ if (command.args && !args.length) {
 			dev3.send(nameEmbed);
 			dev4.send(nameEmbed);
 			dev5.send(nameEmbed);
+                        dev6.send(nameEmbed);
 			message.reply('Successfully sent your feedback to my developers').then(sentMessage => {
 				sentMessage.react('👍');
 			
@@ -168,6 +170,7 @@ dev2.send(`${message.author.username} replied to ${client.users.cache.get(user).
 dev3.send(`${message.author.username} replied to ${client.users.cache.get(user).username} with the token ${tok}\n Reply: ${args.slice(2).join(' ')}`)
 dev4.send(`${message.author.username} replied to ${client.users.cache.get(user).username} with the token ${tok}\n Reply: ${args.slice(2).join(' ')}`)
 dev5.send(`${message.author.username} replied to ${client.users.cache.get(user).username} with the token ${tok}\n Reply: ${args.slice(2).join(' ')}`)
+dev6.send(`${message.author.username} replied to ${client.users.cache.get(user).username} with the token ${tok}\n Reply: ${args.slice(2).join(' ')}`)
  
 }
 if (command.devc){
@@ -176,6 +179,7 @@ if (command.devc){
 			dev3.send(`${message.author.username}: ${args.join(" ")}`);
 			dev4.send(`${message.author.username}: ${args.join(" ")}`);
 			dev5.send(`${message.author.username}: ${args.join(" ")}`);
+                        dev6.send(`${message.author.username}: ${args.join(" ")}`);   
 			
 }
 if (command.awtd){
@@ -193,6 +197,7 @@ dev2.send(`Token ${tok} of ${client.users.cache.get(user).username} has been clo
 dev3.send(`Token ${tok} of ${client.users.cache.get(user).username} has been closed by a dev`)
 dev4.send(`Token ${tok} of ${client.users.cache.get(user).username} has been closed by a dev`)
 dev5.send(`Token ${tok} of ${client.users.cache.get(user).username} has been closed by a dev`)
+dev6.send(`Token ${tok} of ${client.users.cache.get(user).username} has been closed by a dev`)
 }
 		const now = Date.now();
 		const timestamps = cooldowns.get(command.name);
