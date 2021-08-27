7c0df146884da07efbf264d317a6d968d57db57f
Sreyas <73597485+Xhackers7@users.noreply.github.com>
2021-08-27 08:37:12 +0530

    Update dare.js

--- commands/Fun/dare.js
+++ commands/Fun/dare.js
@@ -7,18 +7,12 @@ module.exports = {
 	execute(message) {
 		const Discord = require('discord.js');
 		const client = new Discord.Client();
-		const luhv = require('luhv');
-		let q
-luhv.dare(question => {
-	 q = question
-});
+		
 const r = require('better-tord');
 const qu = r.get_dare();
-let que = [qu,q]
-var wque = Math.floor(Math.random() * que.length)
 const emojis = [':eyes:',':face_with_monocle:',':thinking:',':question:',':grey_question:', ':stuck_out_tongue_winking_eye:',':rolling_eyes:',':see_no_evil:',':zipper_mouth:',':face_with_hand_over_mouth:',':hear_no_evil:',':stuck_out_tongue_winking_eye:',':speak_no_evil:',':no_mouth:']
 var emoji = Math.floor(Math.random() * emojis.length)
-message.channel.send(`**${emojis[emoji]} ${que[wque]} ${emojis[emoji]}**`)
+message.channel.send(`**${emojis[emoji]} ${qu} ${emojis[emoji]}**`)
 console.log(que)
 console.log(wque)
 
