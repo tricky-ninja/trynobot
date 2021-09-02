c043839bee4bf59fed03d1da05c9aac7427533b9
Sreyas <73597485+Xhackers7@users.noreply.github.com>
2021-09-02 06:41:34 +0530

    Update dare.js

--- commands/Fun/dare.js
+++ commands/Fun/dare.js
@@ -13,8 +13,6 @@ const qu = r.get_dare();
 const emojis = [':eyes:',':face_with_monocle:',':thinking:',':question:',':grey_question:', ':stuck_out_tongue_winking_eye:',':rolling_eyes:',':see_no_evil:',':zipper_mouth:',':face_with_hand_over_mouth:',':hear_no_evil:',':stuck_out_tongue_winking_eye:',':speak_no_evil:',':no_mouth:']
 var emoji = Math.floor(Math.random() * emojis.length)
 message.channel.send(`**${emojis[emoji]} ${qu} ${emojis[emoji]}**`)
-console.log(que)
-console.log(wque)
 
     },
     
