024ec8f8eedd6873db748cc4f77b0793e8dfbfbd
Sreyas <73597485+Xhackers7@users.noreply.github.com>
2021-10-06 12:04:15 +0530

    Update credits.js

--- commands/Utility/credits.js
+++ commands/Utility/credits.js
@@ -8,7 +8,7 @@ module.exports = {
 .setColor(6601338)
 .setTitle("This bot is made by")
 .setDescription(`Name: Sreyas, Github: Xhackers7, Discord: TrickyNinja#7497\nGithub: R4ndom1, Discord: BlackHole703#9822\nName: Arshal, Github: arshalabbas, Discord: LoL🐼Panda#3361\nName: Vasim, Github: muhammadvasim, Discord: I AM RADAL#9042`);
-.setFooter(`More people are also involved in this bot, the above mentioned ones are the main devs who did most of the work\n**TrynoBot | Copyright© 2021**
+.setFooter(`More people are also involved in this bot, the above mentioned ones are the main devs who did most of the work\n**TrynoBot | Copyright© 2021**`)
 message.channel.send(nameEmbed);
 	},
 };
