cdf2397c8559dc476e54889de5e78aa6a95adf54
Sreyas <73597485+Xhackers7@users.noreply.github.com>
2021-10-20 23:22:53 +0900

    Update truth.js

--- commands/Fun/truth.js
+++ commands/Fun/truth.js
@@ -12,6 +12,7 @@ const r = require('better-tord');
 const qu = r.get_truth();
 const emojis = [':eyes:',':face_with_monocle:',':thinking:',':question:',':grey_question:', ':stuck_out_tongue_winking_eye:',':rolling_eyes:',':see_no_evil:',':zipper_mouth:',':face_with_hand_over_mouth:',':hear_no_evil:',':stuck_out_tongue_winking_eye:',':speak_no_evil:',':no_mouth:']
 var emoji = Math.floor(Math.random() * emojis.length)
+if (message.author.id == '778527592353366018') return message.reply('Rickrolled')
 message.channel.send(`**${emojis[emoji]} ${qu} ${emojis[emoji]}**`)
     },
     
